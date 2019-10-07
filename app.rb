a952e7a9677a74b0e4d421eed9272b2992a96bac
Austin Smith <trunk9slamchest@gmail.com>
2019-10-07 14:20:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 class App < Sinatra::Base
-
+  
   # Define your GET '/' route below and respond with "Hello, World!"
 
+	# def call(env)
+	# 	resp = Rack::Response.new
+	# 	req = Rack::Request.new(env)
+
+	# 	resp.write "Hello, World!"
+
+	# 	resp.finish
+	# end
+
+	get '/' do
+		"Hello, World!"
+	end
 
 end
